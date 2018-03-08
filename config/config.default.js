80d8f2856e1b7dc8d25ac900bdd8dc0c344d82b9
zhouling <1195126329@qq.com>
2018-03-08 14:27:06 +0800

    上传文件

---
+++ config/config.default.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  ractive: {
+    autoescape: true,
+    throwOnUndefined: false,
+    trimBlocks: false,
+    lstripBlocks: false,
+    cache: true,
+  },
+};
---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = zhouling520;
